784cfc4d1e02e932f8c308d3529d433cb65e964a
Dawid Walczak <dawidwalczak1404@gmail.com>
2023-01-17 21:36:31 +0100

    add new work container color and opacity

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,6 @@ const newTaskContainer = document.querySelector("#new-work-container");
 newTaskBtn.addEventListener("click", () => {
    newTaskContainer.classList.add("visible");
    document.body.classList.add("non-scroll");
-})
+});
+
+// newTaskBtn.click();
