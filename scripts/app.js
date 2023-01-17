e8185da538d83e3ae0eaeb08cffd2fd39f4aa5a0
Dawid Walczak <dawidwalczak1404@gmail.com>
2023-01-17 20:54:16 +0100

    add pop-up for adding new task by clicking button

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const newTaskBtn = document.querySelector("#add-new-work-btn");
+const newTaskContainer = document.querySelector("#new-work-container");
+
+newTaskBtn.addEventListener("click", () => {
+   newTaskContainer.classList.add("visible");
+   document.body.classList.add("non-scroll");
+})
