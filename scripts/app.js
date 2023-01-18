eb76a8b5ca66f183e138d3852b501fc5d7bdca43
Dawid Walczak <dawidwalczak1404@gmail.com>
2023-01-18 01:09:43 +0100

    add new task form, add close form oportunity

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,21 @@
 const newTaskBtn = document.querySelector("#add-new-work-btn");
 const newTaskContainer = document.querySelector("#new-work-container");
+const closeNewTask = document.querySelector("#close-new-work");
 
 newTaskBtn.addEventListener("click", () => {
    newTaskContainer.classList.add("visible");
    document.body.classList.add("non-scroll");
 });
 
-// newTaskBtn.click();
+newTaskContainer.addEventListener("click", closePopUp);
+
+closeNewTask.addEventListener("click", closePopUp);
+
+//functions
+
+function closePopUp(event) {
+   if(event.target === event.currentTarget) {
+      newTaskContainer.classList.remove("visible");
+      document.body.classList.remove("non-scroll");
+   }
+}
