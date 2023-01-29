0b220ac2fba4092d711f5a76e05657846c0a3c90
Dawid Walczak <dawidwalczak1404@gmail.com>
2023-01-29 21:53:16 +0100

    add Task, TaskObject and TasksList classes

--- scripts/add-work.js
+++ scripts/add-work.js
@@ -1,23 +1,33 @@
-export class TaskObject {
+export class Task {
    constructor(name, deadline, time, priority) {
       this.name = name;
       this.deadline = deadline;
       this.time = time;
       this.priority = priority;
+      this.taskObj = new TaskObject(name, deadline, time, priority);
    }
 }
 
-export class TaskList {
+export class TaskObject {
+   constructor(name, deadline, time, priority) {
+      const taskTemplate = document.importNode(document.querySelector("#new-work-template").content, true);
+
+      const todayDate = new Date();
+
+      taskTemplate.querySelector(".work-name").textContent = name;
+      taskTemplate.querySelector(".added-info").textContent = `${todayDate.getDate()}-${todayDate.getMonth()}-${todayDate.getFullYear()}`;
+      taskTemplate.querySelector(".work-name").textContent = name;
+      taskTemplate.querySelector(".work-name").textContent = name;
+      taskTemplate.querySelector(".work-name").textContent = name;
+   }
+}
+
+export class TasksList {
    constructor() {
       this.list = [];
    }
 
-   add(task) {
-      this.list.push(task);
-   }
-
-   remove(task) {
-      const idx = this.list.findIndex(el => el.name === task.name);
-      this.list.splice(idx,1);
+   addTask(name, deadline, time, priority, sortType = "name") {
+      
    }
 }
--- scripts/app.js
+++ scripts/app.js
@@ -2,22 +2,14 @@ import { NewTaskPopUp } from "./new-work-popup.js";
 import * as AddWork from "./add-work.js";
 
 class App {
-   // static init() {
-   //    NewWorkPopUp.addListeners();
-   //    const newTaskForm = document.getElementById("new-work-form");
-   //    const newTaskTemplate = document.importNode(document.querySelector("#new-work").content, true);
-      
-   //    newTaskForm.addEventListener("submit", function(event) {
-   //       event.preventDefault();
-      
-   //       console.log(this.deadline.value);
-      
-   //       NewWorkPopUp.closePopUp(event);
-   //    });
-   // }
-
    static init() {
       NewTaskPopUp.addPopUpHandler();
+   }
+
+   static submitTaskHandler() {
+      const newTaskForm = document.querySelector("#new-work-form");
+
+      newTaskForm.addEventListener("submit", )
    }
 }
 
